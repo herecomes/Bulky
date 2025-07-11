920dbf1d02092640ee07b3ff250dce151d4c3dce
Alik Muhammad <alikm@getaccum.com>
2025-07-11 19:49:43 +0300

    shopping cart controller & view

---
+++ BulkyWeb/Areas/Customer/Controllers/ShoppingCartController.cs
@@ -0,0 +1,34 @@
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace BulkyWeb.Areas.Customer.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ShoppingCartVM ShoppingCartVM { get; set; }
+        public ShoppingCartController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [Area("Customer")]
+        [Authorize]
+        public IActionResult Index()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            ShoppingCartVM = new ShoppingCartVM()
+            {
+                ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u=>u.ApplicationUserId==userId, includeProperties: "Product"),
+                OrderTotal = 0.0
+            };
+
+            return View(ShoppingCartVM);
+        }
+    }
+}
